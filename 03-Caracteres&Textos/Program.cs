c218155b3ca4ab2026178a52a8781b45d2a503d6
Junior Lima <juniorlima.dev@gmail.com>
2023-06-15 21:09:43 -0300

    feat: Criar aula caracteres e textos

---
+++ 03-Caracteres&Textos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Aula 03 - Caracteres e Textos");
+
+char letra, ascllJ, ascllL;
+letra = 'a';
+ascllJ = (char)74;
+ascllL = (char)76;
+
+string frase = "Nem todos os dias são bons, mas há algo bom em cada dia.";
+
+string cursos = @"Cursos disponíveis: 
+- PHP
+- C#
+- Python
+- NoJava";
+
+Console.WriteLine("Char: " + letra);
+Console.WriteLine("Char ASCll: " + ascllJ + ascllL);
+Console.WriteLine(frase);
+Console.WriteLine(cursos);
